b8872adaf392514f9f299d6abc0297f67aead23a
Nathan Krasney <natankrasney@gmail.com>
2022-08-31 08:09:56 +0300

    _document is ok

---
+++ pages/_document.tsx
@@ -0,0 +1,77 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+  return (
+    <Html>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics . should be first in head */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+
+          gtag("config", "${googleAnalyticsId}");
+          `}
+        </Script>
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        {/* SavvyCall start here  */}
+        <Script strategy="lazyOnload">
+          {`
+            window.SavvyCal =
+            window.SavvyCal ||
+            function () {
+              (SavvyCal.q = SavvyCal.q || []).push(arguments);
+            };
+          `}
+        </Script>
+        <Script
+          src="https://embed.savvycal.com/v1/embed.js"
+          strategy="lazyOnload"
+        />
+        <Script strategy="lazyOnload">
+          {`
+        SavvyCal("init", {
+          widget: {
+            enabled: true,
+            backgroundColor: "#DF292D",
+            link: "nathankrasney/14601a74",
+            prompt: "Schedule 1x1 with Nathan",
+          },
+        });
+        `}
+        </Script>
+
+        {/* SavvyCall finish here  */}
+      </body>
+    </Html>
+  );
+}
