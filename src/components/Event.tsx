fd443519f18f7a0a862204ab7c7adb87114de323
Nathan Krasney <natankrasney@gmail.com>
2022-09-05 12:29:00 +0300

    Images in Events

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,8 +1,12 @@
 import styles from "styles/Event.module.css";
 import { FC } from "react";
 import IEvent from "../Interfaces/IEvent";
+import Image from "next/image";
 
 const eventPagePic = "/images/eventPage-pic.jpg";
+const WIDTH_ORIG_PX = 1920;
+const HEIGHT_ORIG_PX = 1280;
+const IMAGE_FACTOR = 4;
 
 const Event: FC<IEvent> = ({ startDt, title, desc, location, url }) => {
   const dateTime = startDt;
@@ -11,7 +15,13 @@ const Event: FC<IEvent> = ({ startDt, title, desc, location, url }) => {
   return (
     <div className={styles.Event}>
       <div className={styles.Event_img_container}>
-        <img src={eventPagePic} alt="event" className={styles.Event_img} />
+        <Image
+          width={WIDTH_ORIG_PX/IMAGE_FACTOR}
+          height={HEIGHT_ORIG_PX/IMAGE_FACTOR}
+          src={eventPagePic}
+          alt="event"
+          className={styles.Event_img}
+        />
         <div className={styles.Event_img_inner_container}>
           <h5 className={styles.Event_date}>{dateTime}</h5>
         </div>
